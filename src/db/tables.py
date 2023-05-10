8350aa53eb1d9ab1546dbe2c277667528ad016a7
Alexander Sachenko <sanyasach@mail.ru>
2023-05-10 22:38:55 +0300

    some

--- src/db/tables.py
+++ src/db/tables.py
@@ -1,7 +1,11 @@
-from sqlalchemy import Column, DateTime, ForeignKey, Integer, String
+import datetime
+
+from sqlalchemy import DateTime, ForeignKey, Integer, String
 from sqlalchemy.ext.compiler import compiles
 from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import mapped_column
 from sqlalchemy.sql import expression
 
 
@@ -19,63 +23,72 @@ def pg_utcnow(element, compiler, **kw):
     return "TIMEZONE('utc', CURRENT_TIMESTAMP)"
 
 
-class SavedUpdatedAt:
+class SavedAt:
     # Datetime of saving to db
 
     # People recommend to use server_default
-    saved_at = Column(DateTime(), default=utcnow(), nullable=False)
+    saved_at: Mapped[datetime.datetime] = mapped_column(default=utcnow())
 
 
-class Advert(Base, SavedUpdatedAt):
-    __tablename__ = "advert"
-
-    advert_id = Column(Integer, primary_key=True)
-    url = Column(String(255), nullable=False)
-    description = Column(String(255), nullable=True)
-    published_at = Column(DateTime(timezone=True), nullable=False)
-    refreshed_at = Column(DateTime(timezone=True), nullable=True)
-    updated_at = Column(DateTime(), default=utcnow(), nullable=False)
-
-    phones = relationship(
-        "Phone", secondary="phone_advert",
-    )
-
-
-class Brand(Base, SavedUpdatedAt):
+class Brand(Base, SavedAt):
     __tablename__ = "brand"
 
-    brand_id = Column(Integer, primary_key=True)
-    url = Column(String(255), nullable=False)
-    label = Column(String(255), nullable=True)
+    brand_id = mapped_column(Integer, primary_key=True)
+    url: Mapped[str]
+    label: Mapped[str]
     # probably it's not necessary field
-    count = Column(Integer)
+    count: Mapped[int]
 
 
-class Model(Base, SavedUpdatedAt):
+class Model(Base, SavedAt):
     __tablename__ = "model"
 
-    model_id = Column(Integer, primary_key=True)
-    label = Column(String(255), nullable=True)
-    brand_id = Column(Integer, ForeignKey(Brand.brand_id))
+    model_id = mapped_column(Integer, primary_key=True)
+    label: Mapped[str]
+    brand_id = mapped_column(ForeignKey("brand.brand_id"))
+
+
+class Advert(Base, SavedAt):
+    __tablename__ = "advert"
+
+    advert_id = mapped_column(Integer, primary_key=True)
+    url: Mapped[str]
+    description: Mapped[str | None]
+    published_at: Mapped[datetime.datetime] = mapped_column(
+        DateTime(timezone=True), default=utcnow()
+    )
+    refreshed_at: Mapped[datetime.datetime | None] = mapped_column(
+        DateTime(timezone=True)
+    )
+    updated_at: Mapped[datetime.datetime] = mapped_column(default=utcnow())
+
+    phones: Mapped[list["Phone"]] = relationship(
+        "Phone", secondary="phone_advert"
+    )
 
 
 class Phone(Base):
     __tablename__ = "phone"
 
-    code = Column(String(8), primary_key=True)
-    number = Column(Integer, primary_key=True)
+    code = mapped_column(String(8), primary_key=True)
+    number = mapped_column(Integer, primary_key=True)
 
-    adverts = relationship(
-        "Advert", secondary="phone_advert", back_populates="phones",
+    adverts: Mapped[list["Advert"]] = relationship(
+        "Advert", secondary="phone_advert"
     )
 
 
 class PhoneAdvert(Base):
     __tablename__ = "phone_advert"
 
-    advert_id = Column(
+    advert_id = mapped_column(
         Integer, ForeignKey("advert.advert_id"), primary_key=True
     )
 
-    code = Column(Integer, ForeignKey("phone.code"), primary_key=True)
-    number = Column(Integer, ForeignKey("phone.number"), primary_key=True)
+    code = mapped_column(Integer, ForeignKey("phone.code"), primary_key=True)
+    number = mapped_column(
+        Integer, ForeignKey("phone.number"), primary_key=True
+    )
+
+    # advert = relationship("Advert", foreign_keys=[advert_id])
+    # phone = relationship("Phone", foreign_keys=[code, number])
