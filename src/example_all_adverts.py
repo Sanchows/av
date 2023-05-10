efa2261e1da124f9ae3c6f43cfb758adfdb4f5b4
Sachenko Alexander <35726682+Sanchows@users.noreply.github.com>
2023-05-10 22:42:26 +0300

    Phones (#6)
    * multiple primary keys in Phone table
    
    * some
    
    * some
    
    * some

--- src/example_all_adverts.py
+++ src/example_all_adverts.py
@@ -1,10 +1,9 @@
 import asyncio
 import itertools
 
-from actions.adverts import create_adverts
+from actions.adverts import clean_adverts, create_adverts
 from actions.brands import clean_brands, create_brands
 from actions.models import clean_models, create_models
-
 
 from parsing.adverts import get_adverts_by_model
 from parsing.brands import get_brands
@@ -27,22 +26,52 @@ def batch(iterable, size):
         yield item
 
 
+from datetime import datetime
+import resource
+
+
 async def main():
-    brands = await get_brands()
+    start = datetime.now()
+    # print("Очищаем объявления из бд")
+    # await clean_adverts()
+    print("Очищаем модели из бд")
+    await clean_models()
+    print("Очищаем брэнды из бд")
     await clean_brands()
+
+    print("Собираем брэнды")
+    brands = await get_brands()
+    print("Сохраняем брэнды в бд")
     await create_brands(brands=brands)
 
+    print("Собираем модели")
     models = await gather_data(
         *(get_models_by_brand(brand=brand) for brand in brands),
     )
-    await clean_models()
+    print("Сохраняем модели в бд")
     await create_models(models=models)
 
-    for batched_models in batch(models, 10):
+    len_models = len(models)
+    remaining = len_models
+    for batched_models in batch(models, 20):
+        print(
+            f"Собираем объявления, еще осталось собрать объявления "
+            f"с {remaining} моделей. Всего {len_models} моделей"
+        )
         adverts = await gather_data(
             *(get_adverts_by_model(model=model) for model in batched_models)
         )
+        print(f"Сохраняем объявления ({len(adverts)} шт.) в бд")
         await create_adverts(adverts=adverts)
+        remaining -= len(batched_models)
+
+    finish = datetime.now()
+    print(f"{finish-start}")
+
+    print(
+        f"Peak memory usage: "
+        f"{resource.getrusage(resource.RUSAGE_SELF).ru_maxrss / 1024} MB"
+    )
 
 
 async def run():
