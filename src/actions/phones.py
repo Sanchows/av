b881f8dc005342ea837050d1d97f1ede35feeec6
Alexander Sachenko <sanyasach@mail.ru>
2023-05-11 16:22:19 +0300

    phone action

--- src/actions/phones.py
+++ src/actions/phones.py
@@ -1,17 +1,17 @@
-from db.dals import BrandDAL
+from db.dals import PhoneDAL
 from db.session import get_session
-from parsing.brands import Brand
+from parsing.phones import Phone
 
 
-async def create_brands(brands: tuple[Brand]):
+async def create_phones(phones: tuple[Phone]):
     async with get_session() as session:
         async with session.begin():
-            brand_dal = BrandDAL(session)
-            await brand_dal.save_brands(brands=brands)
+            phone_dal = PhoneDAL(session)
+            await phone_dal.save_phones(phones=phones)
 
 
-async def clean_brands():
+async def clean_phones():
     async with get_session() as session:
         async with session.begin():
-            brand_dal = BrandDAL(session)
-            await brand_dal.clean_brands()
+            phone_dal = PhoneDAL(session)
+            await phone_dal.clean_phones()
